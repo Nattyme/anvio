4da25b79c33468cf24f36bd4a69b2fa64793cbab
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-04-24 13:17:53 +0300

    swiper base settings

---
+++ src/js/modules/slider-activity.js
@@ -0,0 +1,21 @@
+function sliderActivity() {
+const swiper = new Swiper('#slider-activity', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 1800,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    grabCursor: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '#slider-activity-next',
+      prevEl: '#slider-activity-prev',
+    },
+  });
+
+}
+
+export default sliderActivity;
