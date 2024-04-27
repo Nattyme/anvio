f711673727609290d159ea168bc3277f14df7d69
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-04-28 00:49:52 +0300

    slider-form-order base settings

--- src/js/modules/slider-form-order.js
+++ src/js/modules/slider-form-order.js
@@ -5,20 +5,12 @@ const swiper = new Swiper('#slider-form-order', {
     speed: 1800,
     slidesPerView: 4,
     slidesPerGroup: 1,
-    centeredSlides: true,
-    loop: true,
     spaceBetween: 5,
     grabCursor: true,
 
     keyboard: {
         enabled: true,
       },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '#slider-form-order-next',
-      prevEl: '#slider-form-order-prev',
-    },
   });
 
 }
